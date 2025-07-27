bdf51d8f7c9ae39ca6e7d0a24854ab3df4481b8a
ShneorCode <mm4471661@gmail.com>
2025-07-27 16:53:01 +0300

    Delete server directory

--- server/routes/playersRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPlayersController, upsertPlayerController } from '../controllers/playersController.js';
-
-const router = express.Router();
-
-router.get('/', getPlayersController);
-router.post('/', upsertPlayerController);
-
-export default router;
